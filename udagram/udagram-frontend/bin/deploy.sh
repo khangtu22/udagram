6083c9349cb8c41cadcca61ce98dd5babd8e29ab
khangtv <vankhang226@gmail.com>
2023-10-29 20:40:24 +0700

    Switch store

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive ./www s3://final-udagram/
-aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://final-udagram/
+aws s3 cp --recursive ./www s3://udagram01/
+aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram01/
