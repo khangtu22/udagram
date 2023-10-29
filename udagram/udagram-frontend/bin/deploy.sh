3148f1009778a8e5387b212976ff4f7d8c206547
khangtv <vankhang226@gmail.com>
2023-10-29 19:08:11 +0700

    Fix cp to S3 congig

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive public-read ./www s3://final-udagram/
-aws s3 cp public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://final-udagram/
+aws s3 cp --recursive ./www s3://final-udagram/
+aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://final-udagram/
