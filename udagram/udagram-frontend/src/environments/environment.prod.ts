d33dff99ff882ba8ce528ddb63215c233495e75a
khangtv <vankhang226@gmail.com>
2023-10-29 17:40:06 +0700

    Config circle CI

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udagram-api-dev-2.eba-3f9yaait.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
